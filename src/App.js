eb6642d6cc1dc41e27c9b3b5023533a6d6789392
rbanda1985 <robertbanda85@gmail.com>
2022-03-01 05:15:22 -0600

    ADD: Background image in Home.js and style changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Services from "./components/Services";
@@ -9,22 +9,20 @@ import Education from "./components/Education";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" component={<Home />} element={<Home />} />
-          <Route
-            path="/services"
-            component={<Services />}
-            element={<Services />}
-          />
-          <Route
-            path="/education"
-            component={<Education />}
-            element={<Education />}
-          />
-        </Routes>
-      </BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" component={<Home />} element={<Home />} />
+        <Route
+          path="/services"
+          component={<Services />}
+          element={<Services />}
+        />
+        <Route
+          path="/education"
+          component={<Education />}
+          element={<Education />}
+        />
+      </Routes>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { MDBContainer } from "mdb-react-ui-kit";
+import "./Home.css";
+import image from "../assets/benyamin-bohlouli.jpg";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="image-container text-center d-flex justify-content-center align-items-center">
+        <div className="bg-image">
+          <img
+            src={image}
+            className="img-fluid shadow-4 d-block"
+            id="back-image"
+            alt="home"
+          />
+          <div
+            className="mask"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
+          ></div>
+        </div>
+        <p className="fade-in-text display-5">
+          Experience Stylish Hair Like Never Before
+        </p>
+      </div>
+      <MDBContainer></MDBContainer>
+    </>
+  );
 };
 
 export default Home;
